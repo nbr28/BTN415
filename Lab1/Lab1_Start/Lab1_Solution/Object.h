1649195dd283d711586d5b48bfa17a443b700722
Dr. (James) Elliott Coleshill <elliott.coleshill@senecacollege.ca>
2018-01-16 10:34:02 -0500

    20180116
    Winter 2018 -- Removed the Network Simulator and replaced with basic C++
    coding skills again.

---
+++ Lab1/Lab1_Start/Lab1_Solution/Object.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace W1
+{
+
+}
---
+++ Lab1/Lab1_Start/Lab1_Solution/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include "Object.h"
+using namespace W1;
+
+void print(std::string msg, std::ofstream *ofs)
+{
+	std::cout << msg << std::endl;
+	*ofs << msg << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+	std::string msgToWrite = "";
+	std::ofstream ofs("Output.txt");
+	if (!ofs.is_open())
+		std::cout << "ERROR:  Failed to open file.  Screenshots will be required" << std::endl;
+
+	int Max;
+
+	if (argc < 2)
+	{
+		print("Invalid Number of Arguments", &ofs);
+		return 0;
+	}
+	else
+	{
+		msgToWrite = "Number of Args: " + std::to_string(argc);
+		print(msgToWrite, &ofs);
+		msgToWrite = "Command Line: " + (std::string)argv[0] + " " + (std::string)argv[1];
+		print(msgToWrite, &ofs);
+
+		Max = atoi(argv[1]);
+	}
+	
+	//Object obj(Max, &ofs);
+
+	srand(time(NULL));
+	for (int x = 0; x < Max; x++)
+	{
+		int RandResult = rand() % 10 + 1;
+		//obj += RandResult;
+	}
+
+	//obj.display();
+	
+	return 1;
+}
